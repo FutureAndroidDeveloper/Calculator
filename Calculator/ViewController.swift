4f97aa4169e4cb5d9a2e2864c4cfdeaff75f6535
Kirill Klimenkov <KKlimenkov@qulix.com>
2021-03-25 15:49:06 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Klimenkov, Kirill on 25.03.2021.
+//  Copyright © 2021 Klimenkov, Kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
